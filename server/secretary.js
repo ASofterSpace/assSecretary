a86222fe1c32a9736a813a5160f8c4cb88869d26
Tom Moya Schau <moya@asofterspace.com>
2020-11-09 16:22:42 +0100

    Adjust main page layout to accountant layout

--- server/secretary.js
+++ server/secretary.js
@@ -1,2 +1,35 @@
 window.secretary = {
+
+	onResize: function() {
+
+		var retry = false;
+
+		var body = document.getElementById("body");
+		if (body) {
+			body.style.height = window.innerHeight + "px";
+		} else {
+			retry = true;
+		}
+
+		var mainContent = document.getElementById("mainContent");
+		if (mainContent) {
+			mainContent.style.height = (window.innerHeight - 31) + "px";
+		} else {
+			retry = true;
+		}
+
+		if (retry) {
+			// if we could not fully resize now, then let's do it later...
+			window.setTimeout(function() {
+				window.accountant.onResize();
+			}, 100);
+		}
+	},
+
 }
+
+
+window.addEventListener("resize", window.secretary.onResize);
+
+
+window.secretary.onResize();
