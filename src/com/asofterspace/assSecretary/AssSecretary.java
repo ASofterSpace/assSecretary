c55ef654a15654112ca98517cb0a9d6968cc3114
Moya <moya@asofterspace.com>
2025-12-20 13:59:15 +0100

    Re-enable red glow

--- src/com/asofterspace/assSecretary/AssSecretary.java
+++ src/com/asofterspace/assSecretary/AssSecretary.java
@@ -49,8 +49,8 @@ public class AssSecretary {
 	public final static String FACT_DIR = "../assTrainer/config";
 
 	public final static String PROGRAM_TITLE = "assSecretary (Hugo)";
-	public final static String VERSION_NUMBER = "0.1.1.3(" + Utils.TOOLBOX_VERSION_NUMBER + ")";
-	public final static String VERSION_DATE = "21. October 2020 - 10. December 2025";
+	public final static String VERSION_NUMBER = "0.1.1.4(" + Utils.TOOLBOX_VERSION_NUMBER + ")";
+	public final static String VERSION_DATE = "21. October 2020 - 20. December 2025";
 
 	private static Database database;
 	private static LocationDatabase locationDB;
